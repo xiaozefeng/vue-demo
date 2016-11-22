f4a5c816cfe95768a06caf2e7c67d18fe6645315
xiaozefeng <qq523107430@163.com>
2016-11-22 18:43:46 +0800

    vue-cli脚手架

---
+++ vue-loader-demo/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>Hello Vue</h3>
+    </div>
+    <my-menu></my-menu>
+</template>
+<style>
+
+</style>
+<script>
+    import Menu from './components/Menu.vue'
+
+    export default{
+        data:function(){
+            return {
+                msg:'我是menu'
+            };
+        },
+        components:{
+            'my-menu':Menu
+        }
+    }
+</script>
---
+++ vue-loader-demo/components/Menu.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul>
+        <li>111</li>
+        <li>222</li>
+        <li>333</li>
+    </ul>
+</template>
+<script>
+
+</script>
